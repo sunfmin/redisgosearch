35cf99103df73c07d7800e44054cc956d02184fa
Felix Sun <sunfmin@gmail.com>
2012-08-21 18:31:37 +0800

    strip ,. for split word

--- search.go
+++ search.go
@@ -9,6 +9,7 @@ type Result struct {
 }
 
 type TypeList struct {
+	jsonData string
 }
 
 func (this Result) Type(t string) (r TypeList) {
@@ -27,14 +28,20 @@ func (this *Client) SearchInType(keywords string, indexType string) (r TypeList,
 		args = append(args, this.withnamespace("keywords", word, indexType))
 	}
 
-	fmt.Printf("%+v", args)
+	// fmt.Printf("%+v", args)
 
 	_, err = this.redisConn.Do("SINTERSTORE", args...)
 	if err != nil {
 		return
 	}
 
-	rawRs, err := this.redisConn.Do("GET", this.withnamespace("keywords", targetKey))
-	panic(rawRs)
+	rawRs, err := this.redisConn.Do("SMEMBERS", this.withnamespace("keywords", targetKey, indexType))
+	r.jsonData = "["
+	var stringRs []string
+	iRs := rawRs.([]interface{})
+	for _, row := range iRs {
+		stringRs = append(stringRs, string(row.([]byte)))
+	}
+	fmt.Printf("%+v", stringRs)
 	return
 }
--- segmentation.go
+++ segmentation.go
@@ -2,10 +2,17 @@ package redisgosearch
 
 import (
 	"strings"
+	"regexp"
 )
+
+var tailNonWordRegexp = regexp.MustCompile(`(.*)[^\w]+$`)
 
 func Segment(p string) (r []string) {
 	p = strings.ToLower(p)
-	r = strings.Split(p, " ")
+	r1 := strings.Split(p, " ")
+	for _, word := range r1 {
+		word = tailNonWordRegexp.ReplaceAllString(word, "$1")
+		r = append(r, word)
+	}
 	return
 }
