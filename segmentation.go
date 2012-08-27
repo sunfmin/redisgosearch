335b689932f957f66dbc50ed848b0fbd1dfa6c10
Felix Sun <sunfmin@gmail.com>
2012-08-27 12:02:09 +0800

    better segment

--- segmentation.go
+++ segmentation.go
@@ -2,17 +2,26 @@ package redisgosearch
 
 import (
 	"strings"
-	"regexp"
+	"unicode"
 )
 
-var tailNonWordRegexp = regexp.MustCompile(`(.*)[^\w]+$`)
+func nonWordOrNumbers(w rune) (r bool) {
+	r = !unicode.IsLetter(w) && !unicode.IsDigit(w)
+	return
+}
 
 func Segment(p string) (r []string) {
 	p = strings.ToLower(p)
-	r1 := strings.Split(p, " ")
+	r1 := strings.Fields(p)
 	for _, word := range r1 {
-		word = tailNonWordRegexp.ReplaceAllString(word, "$1")
-		r = append(r, word)
+		r = append(r, strings.TrimFunc(word, nonWordOrNumbers))
+
+		deepSplitWords := strings.FieldsFunc(word, nonWordOrNumbers)
+		if len(deepSplitWords) > 1 {
+			for _, w := range deepSplitWords {
+				r = append(r, w)
+			}
+		}
 	}
 	return
 }
