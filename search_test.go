5f1fce21269aa04d89849032b94c2d50803d6706
Ashwin Purohit <purohit@gmail.com>
2014-06-26 01:48:07 -0400

    Remove test dependencies on bson and mongodb. This is a redis package.

--- search_test.go
+++ search_test.go
@@ -1,16 +1,14 @@
-package tests
+package redisgosearch_test
 
 import (
 	"testing"
 	"time"
 
-	"github.com/sunfmin/redisgosearch"
-	"github.com/sunfmin/mgodb"
-	"labix.org/v2/mgo/bson"
+	"github.com/purohit/redisgosearch"
 )
 
 type Entry struct {
-	ID          bson.ObjectId `bson:"_id"`
+	ID          string
 	GroupID     string
 	Title       string
 	Content     string
@@ -35,7 +33,7 @@ func (attachment *IndexedAttachment) IndexPieces() (r []string, ais []redisgosea
 }
 
 func (attachment *IndexedAttachment) IndexEntity() (indexType string, key string, entity interface{}, rank int64) {
-	key = attachment.Entry.ID.Hex() + attachment.Attachment.Filename
+	key = attachment.Entry.ID + attachment.Attachment.Filename
 	indexType = "files"
 	entity = attachment
 	rank = attachment.Entry.CreatedAt.UnixNano()
@@ -46,13 +44,6 @@ func (attachment *IndexedAttachment) IndexFilters() (r map[string]string) {
 	r = make(map[string]string)
 	r["group"] = attachment.Entry.GroupID
 	return
-}
-
-func (entry *Entry) MakeID() interface{} {
-	if entry.ID == "" {
-		entry.ID = bson.NewObjectId()
-	}
-	return entry.ID
 }
 
 func (entry *Entry) IndexPieces() (r []string, ais []redisgosearch.Indexable) {
@@ -68,7 +59,7 @@ func (entry *Entry) IndexPieces() (r []string, ais []redisgosearch.Indexable) {
 }
 
 func (entry *Entry) IndexEntity() (indexType string, key string, entity interface{}, rank int64) {
-	key = entry.ID.Hex()
+	key = entry.ID
 	indexType = "entries"
 	entity = entry
 	rank = entry.CreatedAt.UnixNano()
@@ -81,14 +72,13 @@ func (entry *Entry) IndexFilters() (r map[string]string) {
 	return
 }
 
+// TestIndexAndSearch requires that Redis be setup in your environment,
+// with the default port.
 func TestIndexAndSearch(t *testing.T) {
-
-	mgodb.Setup("localhost", "redisgosearch")
-
 	client := redisgosearch.NewClient("localhost:6379", "theplant")
 
 	e1 := &Entry{
-		ID:      bson.ObjectIdHex("50344415ff3a8aa694000001"),
+		ID:      "50344415ff3a8aa694000001",
 		GroupID: "Qortex",
 		Title:   "Thread Safety",
 		Content: "The connection http://google.com Send and Flush methods cannot be called concurrently with other calls to these methods. The connection Receive method cannot be called concurrently with other calls to Receive. Because the connection Do method uses Send, Flush and Receive, the Do method cannot be called concurrently with Send, Flush, Receive or Do. Unless stated otherwise, all other concurrent access is allowed.",
@@ -102,7 +92,7 @@ func TestIndexAndSearch(t *testing.T) {
 		CreatedAt: time.Unix(10000, 0),
 	}
 	e2 := &Entry{
-		ID:      bson.ObjectIdHex("50344415ff3a8aa694000002"),
+		ID:      "50344415ff3a8aa694000002",
 		GroupID: "ASICS",
 		Title:   "redis is a client for the Redis database",
 		Content: "The Conn interface is the primary interface for working with Redis. Applications create connections by calling the Dial, DialWithTimeout or NewConn functions. In the future, functions will be added for creating shareded and other types of connections.",
@@ -120,10 +110,7 @@ func TestIndexAndSearch(t *testing.T) {
 		CreatedAt: time.Unix(20000, 0),
 	}
 
-	mgodb.Save("entries", e1)
 	client.Index(e1)
-
-	mgodb.Save("entries", e2)
 	client.Index(e2)
 
 	var entries []*Entry
@@ -151,7 +138,7 @@ func TestIndexAndSearch(t *testing.T) {
 	// sort
 	var sorted []*Entry
 	client.Search("entries", "other", nil, 0, 10, &sorted)
-	if sorted[0].ID.Hex() != "50344415ff3a8aa694000002" {
+	if sorted[0].ID != "50344415ff3a8aa694000002" {
 		t.Error(sorted[0])
 	}
 }
